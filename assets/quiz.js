4edf5a62259770c3b7ed15f3a9a47c7d25b73aff
Pe-ngo <jamespengo95@gmail.com>
2022-05-18 07:41:18 +0300

    add color to submit button

--- assets/quiz.js
+++ assets/quiz.js
@@ -1,11 +1,10 @@
 function studentScore(){
-    //  alert("Excellent")
 
-    var question1=document.theForm.q1.value;
-    var question2=document.theForm.q2.value;
-    var question3=document.theForm.q3.value;
-    var question4=document.theForm.q4.value;
-    var question5=document.theForm.q5.value;
+    var question1=document.theForm.Q1.value;
+    var question2=document.theForm.Q2.value;
+    var question3=document.theForm.Q3.value;
+    var question4=document.theForm.Q4.value;
+    var question5=document.theForm.Q5.value;
     var result= 0;
 
 
@@ -18,31 +17,15 @@ function studentScore(){
 
     var finalScore=result*20;
     if (finalScore>=80){
-        displaytheForm.innerHTML="Your score" + finalScore + "%"
+        displayForm.innerHTML= "Student Score" + " " + finalScore + "%" + " " + "Excellently passed"
     }
 
     else if(finalScore>=50 && finalScore<80){
-        displaytheForm.innerHTML="Your score" + finalScore + "%" 
+        displayForm.innerHTML= "Student Score" + " " + finalScore + "%" + " " + "Fairly passed"
     }
 
     else if(finalScore<50){
-        displaytheForm.innerHTML="Your score" + finalScore + "%"
+        displayForm.innerHTML= "Student Score" + " " + finalScore + "%" + " " + "Scored poorly" + " " + "retake test"
     }
 
-}
-
-function tutorComments(){
-    
-    var displayComments=document.getElementById("checkComments")
-    if (finalScore>=80){
-        displayComments.innerHTML="Excellently passed"
-    }
-    
-    else if(finalScore>=50 && finalScore<80){
-        displayComments.innerHTML="Fairly passed"
-    }
-    
-    else if(finalScore<50){
-        displayComments.innerHTML="Scored poorly" + " " + "Retake test"
-    }
 }
